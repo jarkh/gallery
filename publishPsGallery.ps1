d4b55307f581a0a5f6d926b61c2bcc8923ec0786
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2024-12-18 07:02:53 -0800

    Gallery. (#87)

--- publishPsGallery.ps1
+++ publishPsGallery.ps1
@@ -1,4 +1,5 @@
 Install-Module -Name Microsoft.PowerShell.PSResourceGet -RequiredVersion '1.1.0-rc3' -Force -AllowPrerelease
+Register-PSResourceRepository -PSGallery -Trusted
 
 $InputPath = Join-Path $PSScriptRoot $env:inputpath
 
