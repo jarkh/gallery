102a3c5fedb1e7b08e58d30f7ca4d0b013dc4442
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-12-19 18:42:35 -0600

    Install Required Modules.

--- publishPsGallery.ps1
+++ publishPsGallery.ps1
@@ -1,5 +1,12 @@
 $InputPath = Join-Path $PSScriptRoot $env:inputpath
 
+$ManifestPath = Get-ChildItem -Path $InputPath -Filter *.psd1
+$Manifest = Import-PowerShellDataFile -Path $ManifestPath.FullName
+
+foreach ($RequiredModule in $Manifest.RequiredModules) {
+    Install-Module -Name $RequiredModule -Force -Scope CurrentUser
+}
+
 $PublishSplat = @{
     Path        = $InputPath
     NuGetApiKey = $env:apitoken
